35dd5fd76cd9fb60128757453a1965ccf2476ee0
Subhajeet Dey <dwij.stardust@gmail.com>
2014-12-02 17:49:12 +0530

    Created UI for HomePage

---
+++ app/views/main.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+@section('content')
+<div class="jumbotron">
+	<div class="container">
+		<h1>Welcome to DineBirdie</h1>
+		<p>App to get tweets on various trending resturant bookings and upcomming events</p>
+		<p>
+			<a class="btn btn-primary btn-lg text-center">Sign In With Twitter</a>
+		</p>
+	</div>
+</div>
+@stop
